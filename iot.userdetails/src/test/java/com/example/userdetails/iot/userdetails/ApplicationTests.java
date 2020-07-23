b037f67ddc21bbfd6a187f47a5e1a014fd81cb01
Shanmukhi19 <shanmukhijnv98@gmail.com>
2020-07-23 14:12:26 +0530

    user commit

---
+++ iot.userdetails/src/test/java/com/example/userdetails/iot/userdetails/ApplicationTests.java
@@ -0,0 +1,14 @@
+//package com.example.userdetails.iot.userdetails;
+//
+//import org.hibernate.validator.internal.IgnoreForbiddenApisErrors;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@IgnoreForbiddenApisErrors
+//class ApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
